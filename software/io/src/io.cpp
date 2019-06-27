a54030fe85229f79e6a55000ad0c06b14fcf81d7
hackdiguo <hundun4.66920@gmail.com>
2019-06-27 17:34:52 +0800

    修改2

--- software/io/src/io.cpp
+++ software/io/src/io.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 //程序引用aris库的头文件
 #include <aris.hpp>
 #include<string>
@@ -16,13 +16,14 @@ int main()
 	
 	//打印主站扫描的从站个数
     std::cout<<"slave num:"<<mst.slavePool().size()<<std::endl;
-	
+
+	auto&cs = aris::server::ControlServer::instance();
 	//1、主站对象mst的成员函数setControlStrategy()创建一个实时线程。其形参即是被调用的实时函数，在实时核中每1ms被调用1次
 	//2、被调用函数可以实现主站与从站之间的数据交互，打印服务，log服务。(本例以读、写IO信号，并打印为例)
     mst.setControlStrategy([&]()
     {
         static int count{ 0 };	//count用于计数本函数setControlStrategy()执行的次数
-        static std::uint8_t value{ 0x01 };	
+        static std::uint8_t value{ 0x01 };
 		
         if (++count % 1000 == 0)	//实时核执行一次周期是1ms，每执行1000次，执行本if条件内的语句
         {
@@ -36,18 +37,18 @@ int main()
 			
 			//1、成员函数ecSlavePool()创建从站vector，在实时核中要使用ecSlavePool()，在非实时核使用SlavePool()，at(1)表示第2个从站，即EtherCAT IO板卡在物理连接层面属于第二个从站
             //2、writePdo是写函数，第一个形参是index，第二个形参是subindex，第三个形参写DO的数值，第四个形参表示写操作的bit数
-			mst.ecSlavePool().at(1).writePdo(0x7001,0x02,&value,8);
+			mst.slavePool().at(1).writePdo(0x7001,0x02,&value,8);
         }
     });
 	
 	//启动实时线程
-    mst.start();
+    cs.start();
 	
 	//非实时线程睡眠100秒钟
     std::this_thread::sleep_for(std::chrono::seconds(100));
 	
 	//关闭实时线程
-    mst.stop();
+    cs.stop();
 
 	return 0;
 }
